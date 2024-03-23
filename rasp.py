50782746bb63ba4de39b712c5571c34b6e272346
raacee <raceldamechli@outlook.fr>
2024-03-23 21:51:15 +0100

    added blinks led function + added keyboard interrupt

--- rasp.py
+++ rasp.py
@@ -6,46 +6,71 @@ from draw import edge_over_threshold, scale_keypoints, draw_edges_angles, draw_k
 import cv2 as cv
 import math
 
+camera = Picamera2()
+threshold = 0.35
+led = LED(17)
+
 
 def main(show=False):
-    camera = Picamera2()
-    threshold = 0.35
-    led = LED(17)
+    """
+    Main function for Raspberry Pi posture detection
+    Args:
+        show: boolean flag to show captured video in open cv window
 
+    Returns:
+        None
+    """
     camera.start()
-    while True:
-        # Capture frame-by-frame
-        frame = camera.capture_array()
-        frame = frame[:, :, :3]
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    if show:
+        while True:
+            # Capture frame-by-frame
+            frame, keypoints_locs, scores = led_detect()
 
-        resized_frame = cv.resize(frame, (192, 192))
-        keypoints_with_scores = scale_keypoints(get_keypoints(resized_frame.reshape(1, *resized_frame.shape)))
-
-        keypoints_locs = keypoints_with_scores[:, :2].astype(int)
-        scores = keypoints_with_scores[:, 2]
-
-        for edge1, edge2 in ANGLES_TO_MONITOR:
-            # Create vectors for each edge in the pair
-            if edge_over_threshold(edge1, scores, threshold) and edge_over_threshold(edge2, scores, threshold):
-                angle = angle_from_keypoints(keypoints_with_scores, edge1, edge2)
-                if math.sin(angle) > 0.5:
-                    led.blink(0.1, 0.1, 15)
-                else:
-                    led.off()
-
-        if show:
             frame = draw_keypoints(frame, keypoints_locs, scores, threshold)
-            frame, drawn_edges = draw_edges_angles(frame, keypoints_locs, scores)
+            frame, drawn_edges = draw_edges_angles(frame, keypoints_locs, scores, threshold)
             frame = draw_edges_lines(frame, drawn_edges, keypoints_locs, scores, threshold)
             cv.imshow('main', cv.resize(frame, (640, 480)))
 
             if cv.waitKey(1) == ord('q'):
                 break
+    else:
+        while True:
+            try:
+                led_detect()
+            except KeyboardInterrupt:
+                break
 
     # When everything done, release the capture
     camera.stop()
     cv.destroyAllWindows()
+
+
+def led_detect():
+    """
+    Blinks led according to angles measured between monitored edges
+    Returns:
+        None
+    """
+    frame = camera.capture_array()
+    frame = frame[:, :, :3]
+    frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+    resized_frame = cv.resize(frame, (192, 192))
+    keypoints_with_scores = scale_keypoints(get_keypoints(resized_frame.reshape(1, *resized_frame.shape)))
+
+    keypoints_locs = keypoints_with_scores[:, :2].astype(int)
+    scores = keypoints_with_scores[:, 2]
+
+    for edge1, edge2 in ANGLES_TO_MONITOR:
+        # Create vectors for each edge in the pair
+        if edge_over_threshold(edge1, scores, threshold) and edge_over_threshold(edge2, scores, threshold):
+            angle = angle_from_keypoints(keypoints_with_scores, edge1, edge2)
+            if math.sin(angle) > 0.5:
+                led.blink(0.1, 0.1, 15)
+            else:
+                led.off()
+
+    return frame, keypoints_locs, scores
 
 
 if __name__ == "__main__":
